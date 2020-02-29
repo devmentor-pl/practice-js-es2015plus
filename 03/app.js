768157d371bb7bc2775ddd83c26b3fc4c9209b04
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-02-29 19:25:27 +0000

    Zadanie 03 zrobione

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,21 @@
 import {files} from './data.js';
 
+console.log(files);
+let sum = 0;
+
+files.forEach( element => {
+  const {name, size} = element;
+  console.log(name, size);
+  const {length, unit = 'B'} = size;
+  if(unit === 'B') {
+    sum+=length;
+  } else if(unit === 'KB') {
+    sum+=(length*1024);
+  } else if(unit === 'MB') {
+    sum+=(length*1024*1024);
+  } else if(unit === 'GB') {
+    sum+=(length*1024*1024*1024);
+  }
+  console.log(length, unit);
+})
+console.log(sum/(1024*1024), 'MB');
