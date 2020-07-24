8b5d726b90c81d4c193d0b7a2986a6d020949aab
Martin Gawlyta <mlvrkhn@Martins-MacBook-Pro.local>
2020-07-24 02:33:17 +0200

    v 2.6

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,27 @@
-import {files} from './data.js';
+import {
+  files
+} from './data.js';
 
+// create the sum variable
+let sumMB = 0;
+
+//get to each file length
+files.forEach(el => {
+
+  const [unit] = files;
+  console.log(unit);
+
+  if (!el.size.unit) {
+    sumMB += (el.size.length / 1024 / 1024);
+  }
+  if (el.size.unit === 'KB') {
+    sumMB += (el.size.length / 1024);
+  }
+  if (el.size.unit === 'MB') {
+    sumMB += (el.size.length);
+  }
+  if (el.size.unit === 'GB') {
+    sumMB += (el.size.length * 1024);
+  }
+});
+console.log(sumMB.toFixed(2));
