9f86f2c6ac40eac797b9a044f93aea4e4419a8a0
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-02 11:47:08 +0100

    Create admin class that extends User class Task 04

--- 04/User.js
+++ 04/User.js
@@ -1,11 +1,14 @@
 export default class User {
-    constructor( {login = '', password = ''} ) {
+    constructor({
+        login = '',
+        password = ''
+    }) {
         this.login = login;
         this.password = password;
     }
 
     register() {
-        if(this.isValid()) {
+        if (this.isValid()) {
             console.log('done');
             return true;
         }
@@ -15,7 +18,7 @@ export default class User {
     }
 
     isValid() {
-        if(this.isLoginCorrect() && this.isPasswordCorrect()) {
+        if (this.isLoginCorrect() && this.isPasswordCorrect()) {
             return true;
         }
 
@@ -23,7 +26,7 @@ export default class User {
     }
 
     isLoginCorrect() {
-        if(this.login.includes('@')) {
+        if (this.login.includes('@')) {
             return true;
         }
 
@@ -31,7 +34,7 @@ export default class User {
     }
 
     isPasswordCorrect() {
-        if(this.password.length >= 6) {
+        if (this.password.length >= 6) {
             return true;
         }
 
