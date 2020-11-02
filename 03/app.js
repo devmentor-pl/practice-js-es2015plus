5d832b14b283bd415f0ffaeafb8d39cbe25e9db2
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-02 11:37:05 +0100

    Calculate sum of file length Task 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,34 @@
-import {files} from './data.js';
+import {
+    files
+} from './data.js';
 
+let sum = 0;
+
+const convertSize = (obj) => {
+    let size;
+    switch (obj.size.unit) {
+        case 'KB':
+            size = obj.size.length * 1024;
+            break;
+
+        case 'MB':
+            size = obj.size.length * 1024 * 1024;
+            break;
+
+        case 'GB':
+            size = obj.size.length * 1024 * 1024 * 1024;
+            break;
+
+        default:
+            size = obj.size.length;
+            break;
+    }
+    return size;
+}
+
+files.forEach(file => {
+    let size = convertSize(file)
+    sum += size;
+})
+
+console.log(sum);
