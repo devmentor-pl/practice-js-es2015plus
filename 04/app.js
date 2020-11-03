f230bd5616c912605ad004a0b69d9c3d0d22ff09
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-11-03 10:51:05 +0100

    Fix Admin class in Task 04

--- 04/app.js
+++ 04/app.js
@@ -1,4 +1,5 @@
 import User from './User.js';
+import Admin from './Admin.js';
 
 const userData = {
     login: 'user@devmentor.pl',
@@ -12,18 +13,6 @@ const adminData = {
 const user = new User(userData);
 user.register();
 
-class Admin extends User {
-    constructor(login, password) {
-        super(login, password);
-    }
-    isPasswordCorrect() {
-        if (this.password.length >= 10) {
-            return true;
-        }
-
-        return false;
-    }
-}
 
 const admin1 = new Admin(userData);
 admin1.register();
