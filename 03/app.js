c3f355c5ec3051d1490271857bef50d8f618ce2d
gregor232 <ktotok@gmail.com>
2020-12-16 17:48:47 +0100

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,30 @@
 import {files} from './data.js';
 console.log(files)
-const [
-    {
-        name: value,
-        size: {
-            length
-        }
+let sum = 0;
+
+const sizeSum = () => {
+        files.forEach(f => {
+            const {name, size} = f;
+            
+            unitCheck(size)
+            
+            sum = sum + size.length;
+            
+
+        })
+
+};
+const unitCheck = (size) => {
+    if (size.unit === 'KB') {
+        size.length = size.length * 1024;
     }
-] = files;
-console.log(size)
+    if (size.unit === 'MB') {
+        size.length = size.length * 1048576;
+    }
+    if (size.unit === 'GB') {
+        size.length = size.length * 1073741824;
+    }
+}
+sizeSum()
+const filesSize = sum + " B";
+console.log(filesSize)
