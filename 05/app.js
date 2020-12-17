77a6def5645f4a3c536977286e2fc27d095a5ed2
gregor232 <ktotok@gmail.com>
2020-12-17 19:12:08 +0100

    task 05 done

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,8 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
+
 
 const features = {
     skills: 10, 
@@ -14,3 +18,13 @@ const task = {
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+const junior = new Junior(30, 30, 30, task);
+const mid = new Mid(undefined, undefined, undefined, task);
+const senior = new Senior();
+
+
+
+
+console.log(junior)
+console.log(mid)
+console.log(senior)
---
+++ 05/classes/Mid.js
@@ -0,0 +1,80 @@
+class Mid {
+    constructor(skills, experience, willingness, obj) {
+        this.skills = this.isNumber(skills) ? skills : this.getSkillsRandomNumber();
+        this.experience = this.isNumber(experience) ? experience : this.getExperienceRandomNumber();
+        this.willingness = this.isNumber(willingness) ? willingness : this.getWillingnessRandomNumber();
+        this.time = this.isNumberAndSize(obj) ? this.getApproximateTimeTaskDoneInHours( obj ) : undefined;
+    }
+    isNumber(value) {
+        if(
+            typeof value === 'number'
+            && value === value // NaN === NaN => false
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+    isNumberAndSize(value) {
+        console.log(value)
+        //const {a, b} = value;
+        //console.log(a, b)
+        // probowalem tu destrukturyzacji , nie wiem dlaczego sie nmie powiodlo.
+        if(
+            value && typeof value.difficult === 'number' && typeof value.size === 'number' && value.size <= 3 && value.size >= 1
+        )
+        {    
+            return true;
+        }
+        
+
+        return false;
+    }
+    getSkillsRandomNumber() {
+        return this.getRandomNumber(...this.getMinMaxSkills());
+    }
+    getExperienceRandomNumber() {
+        return this.getRandomNumber(...this.getMinMaxExperience())
+    }
+
+    getWillingnessRandomNumber() {
+        return this.getRandomNumber(...this.getMinMaxWilligness())
+    }
+
+    getMinMaxSkills() {
+        return [0, 30];
+    }
+
+    getMinMaxExperience() {
+        return [0, 30];
+    }
+
+    getMinMaxWilligness() {
+        return [30, 70]
+    }
+
+    
+    getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+    getApproximateTimeTaskDoneInHours( {difficult, size} ) {
+        let counter = 0;
+        let time = 0;
+        let rand;        
+
+        const done = 5 * difficult * size;
+        const chance = ( (0.5 * this.skills) + (0.3 * this.experience) + (0.2 * this.willingness)) / 3;
+        while(counter < done && time < 1000) {
+            rand = this.getRandomNumber(0, 100);
+            if(rand <= chance) {
+                counter++;
+            }
+
+            time += 0.25;
+        }
+
+        return time;
+    }
+}
+export default Mid;
+
