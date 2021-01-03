34a78212d71b8d49c673d3a323e106a47eed343f
Ewa Skórska <ewaskorska94@gmail.com>
2021-01-03 20:44:55 +0000

    Extend User adding Admin

---
+++ 04/Admin.js
@@ -0,0 +1,11 @@
+import User from './User.js';
+
+export default class Admin extends User {
+    constructor( {login = '', password = ''} ) {
+        super( {login, password} );
+    }
+
+    isPasswordCorrect() {
+        return this.password.length >= 10;
+    }
+}
