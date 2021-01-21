6480ec149d49a8a740eef1d4884f28393d623b1a
Marek Mitko <mitko.email@gmail.com>
2021-01-21 01:28:44 +0100

    ex 01 is done

--- 01/app.js
+++ 01/app.js
@@ -5,3 +5,9 @@ const message = {
 const { text } = { ...message };
 
 console.log( text );
+
+
+//?dev W tym zadaniu należało na początek w terminalu wpisać: npm i webpack@4 webpack-cli@3 -D
+// czy w sumie to wystarczyło wpisać npm i bo w pliku package.json mieliśmy zdefiniowane wersje? 
+
+//?dev //infO Sprawdzić dlaczego DevTools failed to load SourceMap: Could not load content for webpack:///node_modules/sockjs-client/dist/sockjs.js.map: HTTP error: status code 404, net::ERR_UNKNOWN_URL_SCHEME 
