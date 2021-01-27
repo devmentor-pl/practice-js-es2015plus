03f608d87cc09f6efc2e6d016cb4bbe73873baef
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-27 16:52:04 +0100

    Zad 03 part I

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,18 @@
 import {files} from './data.js';
 
+
+const { name,
+        size: {unit: 'B'},
+    } = files;
+
+console.log(name);
+
+
+const changeUnit = unit => {
+
+}
+
+// const b = 1024;
+// const kb = b * kb;
+// const mb = b * mb;
+// const gb = b * gb;
