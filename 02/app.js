15e4fa533aa44db20b4b82a40d025b0627d55e0e
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-05-21 23:05:36 +0200

    Task 01 and 02
    Task 01 and 02

---
+++ 02/app.js
@@ -0,0 +1,36 @@
+
+const total = document.querySelector('.cart__total-price');
+const inputs = [... document.getElementsByClassName('cart__quantity')];
+const button = document.querySelector('.cart__btn-calculate');
+
+button.addEventListener('click', e => {
+    e.preventDefault()
+    let totalPrice = 0;
+
+    inputs.forEach(el => {
+        const {value: quantity, dataset: {price: unitPrice}} = el;
+        totalPrice += quantity * unitPrice;
+    });
+
+    total.innerText = numberSeparator(`${totalPrice} `);
+    total.style.color = 'blue';
+    button.style.backgroundColor = 'green';
+});
+
+inputs.forEach(e => {
+    e.value = '';
+    e.setAttribute('autocomplete','off');
+    e.setAttribute('onkeydown','return numberOnly(event)');
+    console.log(e)
+});
+
+// only 0-9 characters
+const numberOnly = (e => {
+	let key = e.keyCode;
+	return ((key >= 48 && key <= 57) || key == 8);
+});
+
+// separator 1000
+const numberSeparator = (e => {
+    return e.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+});
