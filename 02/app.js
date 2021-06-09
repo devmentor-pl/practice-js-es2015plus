508dcee9eda38a3e3588c5339834d165a36ca330
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-06-09 09:04:23 +0200

    Task 01 and 02

---
+++ 02/app.js
@@ -0,0 +1,43 @@
+const init = () => {
+    btn = document.querySelector(".cart__btn-calculate");
+    btn.addEventListener("click", getValue);
+};
+
+document.addEventListener("DOMContentLoaded", init);
+
+const getValue = () => {
+
+    const inputEl = document.getElementsByClassName("cart__quantity");
+    const inputElArr = [ ...inputEl];
+    let sum = 0;
+
+    inputElArr.forEach( (element) => {
+        
+        const {
+            dataset: { price: price },
+            value: quantity,
+        } = element;
+        
+        const multi = getSum(price, quantity)
+        sum = sum + multi
+    
+    });
+
+    printTotalValue(sum)
+};
+
+const getSum = (price, quantity) => { 
+
+    if(quantity < 0 || isNaN(quantity)) {
+        alert("Wpisz poprawną ilość!");
+    };
+
+    return parseInt(price) * parseInt(quantity);
+};
+
+const printTotalValue = (sum) => {
+
+    const totalPriceEL = document.querySelector(".cart__total-price");
+    totalPriceEL.innerText = sum;
+    
+};
