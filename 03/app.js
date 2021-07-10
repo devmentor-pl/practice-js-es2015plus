d65a2a1710059f87c2587cb2559cd9c0e901d671
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-07-10 16:11:26 +0200

    Tasks 01/02/03/04/05 - ready

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,42 @@
-import {files} from './data.js';
+import {
+    files
+} from './data.js';
 
+const init = () => getData(files);
+
+const sumSizes = (el, length) => {
+    let sum = 0;
+    switch (el) {
+        case 'KB':
+            sum = sum + length * 1024;
+            break;
+        case 'MB':
+            sum = sum + length * (1024 ** 2)
+            break;
+        case 'GB':
+            sum = sum + length * (1024 ** 3);
+            break;
+        default:
+            sum = sum + length;
+            break;
+    };
+    return sum;
+}
+
+const getData = (args) => {
+    let result;
+    for (const key in args) {
+        const {
+            size: {
+                length,
+                unit,
+            },
+        } = args[key];
+        result = sumSizes(unit, length);
+    };
+    return result;
+};
+
+document.addEventListener('DOMContentLoaded', init);
+
+console.log(`Pliki zajmują łącznie: ${init()} b`);
