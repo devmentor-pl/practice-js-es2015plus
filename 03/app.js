11651bfdddf89cb5c4cd5f8ce3101c336774bd86
Malgorzata Kowacka <kowackag@gmail.com>
2021-07-13 09:16:35 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,38 @@ import {
     files
 } from './data.js';
 
-const [file1, file2, file3, file4] = files;
+const countSum = () => {
+    let sumFiles = 0;
+    files.forEach(file => {
+        const {
+            size: {
+                length = 0,
+                unit = 'B'
+            }
+        } = file;
+        sumFiles += changeSize(length, unit)
+    });
+    console.log(`Rozmiar wszystkich plików to: ${sumFiles}B`);
+}
 
-const changeSize = 
+const changeSize = (length, unit) => {
+    let sizeInBytes = 0;
+    switch (unit) {
+        case 'B':
+            sizeInBytes = length;
+        case 'KB':
+            sizeInBytes = length * 1024;
+            break;
+        case 'MB':
+            sizeInBytes = length * 1024 * 1024;
+            break;
+        case 'GB':
+            sizeInBytes = length * 1024 * 1024 * 1024;
+            break;
+        default:
+            break;
+    }
+    return sizeInBytes;
+}
+
+document.addEventListener('DOMContentLoaded', countSum);
