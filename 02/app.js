27ddf745fae023a227365c4695b702eba25ee3ec
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-08-05 12:06:17 +0200

    .

---
+++ 02/app.js
@@ -0,0 +1,12 @@
+const inputList = document.getElementsByClassName('cart__quantity')
+console.log(inputList);
+
+const inputListArr = [...inputList];
+console.log(inputListArr);
+
+const sum = (a,b) => {
+    return a * b;
+}
+
+
+// const sum = (a,b) => a * b;
