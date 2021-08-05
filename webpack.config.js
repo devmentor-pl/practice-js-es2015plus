1d96ecc10b7ea4b79bece8b3c4760eb4a9bfaade
Mateusz Bogolubow <mateusz@bogolubow.com>
2021-08-05 12:37:44 +0200

    Fix problem with HMR (autoreload)

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
         filename: 'app.min.js',
         // definiuję nazwę pliku wyjściowego
     },
+    target: "web", 
+    // niezbędne do uruchomienia automatycznego odświeżania z webpack-dev-server@3
+    // https://github.com/webpack/webpack-dev-server/issues/2758
     module: {
         rules: [
             {
