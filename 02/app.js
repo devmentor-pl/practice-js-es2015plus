13ac2439d108c5d97901e091e079ddcdeb60209a
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-08-07 19:02:43 +0200

    Task 02- done

--- 02/app.js
+++ 02/app.js
@@ -1,12 +1,55 @@
-const inputList = document.getElementsByClassName('cart__quantity')
-console.log(inputList);
+const init = () => {
 
-const inputListArr = [...inputList];
-console.log(inputListArr);
+    const calculateButton = document.querySelector('.cart__btn-calculate');
 
-const sum = (a,b) => {
-    return a * b;
+    calculateButton.addEventListener('click' , totalPrice);
 }
 
+const totalPrice = () => {
 
-// const sum = (a,b) => a * b;
+    const inputsList = document.getElementsByClassName('cart__quantity');
+    let sum = 0;
+
+    [...inputsList].forEach( element => {
+
+        const values = getValuesFromItem(element);
+        const calculetedSum = calcuteSum(values);
+
+        sum += calculetedSum;
+    } )
+    setTotalPrice(sum);
+}
+
+const getValuesFromItem = (item) => {
+
+    const {
+        value : quantity,
+        dataset: {price},
+    } = item
+
+    return {
+        quantity ,
+        price
+    };
+}
+
+const calcuteSum = (item) => {
+
+    const quantity = parseInt(item.quantity)
+    const price = parseInt(item.price)
+
+    if( quantity.length !== 0 && !Number.isNaN(quantity) && quantity >= 0 ) {
+        return quantity * price;
+    }
+    else {
+        alert('Enter the correct quantity');
+    }
+}
+
+const setTotalPrice = (item) => {
+
+    const totalPrice = document.querySelector('.cart__total-price');
+    totalPrice.innerHTML = item;
+}
+
+document.addEventListener('DOMContentLoaded' , init)
