97e6a676ecffef30d4c82e032bee08e1ffde25c6
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-09-10 01:55:23 +0200

    closes task 5

--- 05/app.js
+++ 05/app.js
@@ -1,16 +1,31 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
-    skills: 10, 
-    experience: 10, 
+    skills: 10,
+    experience: 10,
     willingness: 10,
 }
 
 const task = {
-    difficult: 1,
-    size: 1,
+    difficult: 3,
+    size: 3,
 }
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
-console.log(time);
+const programmer = new Programmer(features);
+const programmerTime = programmer.getApproximateTimeTaskDoneInHours(task);
+console.log(`Programmer: ${programmerTime}`);
+
+const junior = new Junior();
+const juniorTime = junior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Junior: ${juniorTime}`);
+
+const mid = new Mid();
+const midTime = mid.getApproximateTimeTaskDoneInHours(task);
+console.log(`Mid: ${midTime}`);
+
+const senior = new Senior();
+const seniorTime = senior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Senior: ${seniorTime}`);
