284900888a627be8fec0971215d0d14374ee432c
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-10-18 20:41:28 +0200

    Problem with task 5 - import/export

---
+++ 05/classes/Junior.js
@@ -0,0 +1,15 @@
+import Programmer from './classes/Programmer';
+
+export default class Junior extends Programmer {
+	getMinMaxSkills() {
+		return [0, 30];
+	}
+
+	getMinMaxExperience() {
+		return [0, 30];
+	}
+
+	getMinMaxWilligness() {
+		return [30, 70];
+	}
+}
