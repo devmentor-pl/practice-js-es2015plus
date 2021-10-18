593a153d422352490d30216f735fcfa4653f325e
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-10-18 19:31:10 +0200

    Finish task 4

--- 04/app.js
+++ 04/app.js
@@ -1,7 +1,23 @@
 import User from './User.js';
 
-const userData = { login: 'user@devmentor.pl', password: '123456' }
-const adminData = { login: 'admin@devmentor.pl', password: '1234567890' }
+const userData = { login: 'user@devmentor.pl', password: '123456' };
+const adminData = { login: 'admin@devmentor.pl', password: '1234567890' };
 
-const user = new User( userData );
+const user = new User(userData);
 user.register();
+
+class Admin extends User {
+	isPasswordCorrect() {
+		if (this.password.length >= 10) {
+			return true;
+		}
+
+		return false;
+	}
+}
+
+const admin1 = new Admin(userData);
+admin1.register();
+
+const admin2 = new Admin(adminData);
+admin2.register();
