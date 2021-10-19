88b78c96d2d17d6ce6d5b7631ded687393b6e945
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-10-19 15:48:27 +0200

    Correct task 4

--- 04/app.js
+++ 04/app.js
@@ -1,20 +1,11 @@
 import User from './User.js';
+import Admin from './Admin.js';
 
 const userData = { login: 'user@devmentor.pl', password: '123456' };
 const adminData = { login: 'admin@devmentor.pl', password: '1234567890' };
 
 const user = new User(userData);
 user.register();
-
-class Admin extends User {
-	isPasswordCorrect() {
-		if (this.password.length >= 10) {
-			return true;
-		}
-
-		return false;
-	}
-}
 
 const admin1 = new Admin(userData);
 admin1.register();
