894473a703d15c8a8e47b819962a93644124a879
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-10-27 14:12:17 +0200

    Second task done

--- 02/app.js
+++ 02/app.js
@@ -1,29 +1,47 @@
-const { target } = require("../webpack.config");
+// document.addEventListener('DOMContentLoaded', init);                 <-- jak zrobić taki zapis za pomocą funkcji strzałkowych?
 
-let test = document.querySelectorAll('.cart__item');
-let totalPrice = document.querySelector('.cart__total-price');
-let amount = [...document.getElementsByClassName('cart__item')];
+// const init = () => {
+//     xyz
+// }
+
+// btn.addEventListener('click', sumUp);
+
+ // const sumUp = (ev) => {
+    //     console.log(ev.target);
+    // }
+
+// });
+
+    // btn.addEventListener('click', sumUp);                            <-- dlaczego zapis w ten sposób nie zadziałał?
+
+    // const sumUp = (ev) => {
+    //     console.log(ev.target);
+    // }
+
+
+const inputs = [...document.getElementsByClassName('cart__quantity')];
+const totalPrice = document.querySelector('.cart__total-price');
 const btn = document.querySelector('.cart__btn-calculate');
 
-console.log(test);
-// test.forEach(function(el){
-//     console.log(el.dataset);
-// })
 
-// test.forEach(function(el){
-//     el.addEventListener('click', function(ev){
-//         console.log(this.dataset.price)
-//     })
-// })
+btn.addEventListener('click', () => {
+    sum = 0;
+    let sumArr = [];
+    inputs.forEach((el) => {
 
-amount.forEach(function(el){
-    console.log(el);
-    el.addEventListener('change', function(ev){
-        console.log(ev.target.value);
+        const {
+            dataset: {price:singlePrice},
+            value:itemAmount
+        } = el;
 
+        sum = parseFloat(singlePrice) * parseFloat(itemAmount);
+        sumArr.push(sum);
     });
+
+    total = sumArr.reduce((a,b) => {
+        return a+b;
+    },0)
+
+    // console.log(total);
+    totalPrice.innerText = total;
 });
-
-
-console.log(totalPrice);
-console.log(btn);
