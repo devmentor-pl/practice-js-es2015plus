c7f692b5d846a7ba7d525af3fd8754d17686346f
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-10-28 09:30:10 +0200

    Fourth task done

--- 04/app.js
+++ 04/app.js
@@ -3,5 +3,27 @@ import User from './User.js';
 const userData = { login: 'user@devmentor.pl', password: '123456' }
 const adminData = { login: 'admin@devmentor.pl', password: '1234567890' }
 
+class Admin extends User {
+    // constructor(userData, adminData){            <-- jak można to rozwiązać za pomocą tego? czy jest to w ogóle potrzebne?
+    //     super(userData, adminData);              <-- jak można to rozwiązać za pomocą tego? czy jest to w ogóle potrzebne?
+    // }
+    
+    isPasswordCorrect() {
+        if(this.password.length >= 10) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
 const user = new User( userData );
 user.register();
+
+const admin1 = new Admin( userData );
+admin1.register(); // error
+
+const admin2 = new Admin( adminData );
+admin2.register(); // done
