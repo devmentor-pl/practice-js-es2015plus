98e94fdf70b5c91c29d3b4a1e1bfce3bcdc9d027
Krzysztof <krzysiek.swol@gmail.com>
2022-01-09 19:53:36 +0100

    task o5 finished, add class junior, mid, senior

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from "./classes/Junior";
+import Mid from "./classes/Mid";
+import Senior from "./classes/Senior";
 
 const features = {
     skills: 10, 
@@ -14,3 +17,15 @@ const task = {
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+
+const junior = new Junior();
+const timeJuniorTask = junior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Junior: ${timeJuniorTask}`);
+
+const mid = new Mid();
+const timeMidTask = mid.getApproximateTimeTaskDoneInHours(task)
+console.log(`Mid: ${timeMidTask}`);
+
+const senior = new Senior();
+const timeSeniorTask = senior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Senior: ${timeSeniorTask}`);
