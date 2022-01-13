37ce3cfbbb54e5ba7c135a9517e9dbbcd9e4c980
Ewelina <ewelina@ekopacz.pl>
2022-01-13 13:46:25 +0100

    finish task 02

---
+++ 02/app.js
@@ -0,0 +1,34 @@
+const inputElList = document.getElementsByClassName('cart__quantity');
+const numberItem = [ ...inputElList];
+const calculateButton = document.querySelector('.cart__btn-calculate');
+const totalPirceArea = document.querySelector('.cart__total-price');
+
+let numberArray = [];
+let priceArray = [];
+let totalCost = 0;
+
+numberItem.forEach(item => item.addEventListener('change',getNumberAndPrice));
+
+function getNumberAndPrice(e){
+    numberArray.push(e.target.value);
+    priceArray.push(e.target.dataset.price);
+
+    console.log(numberArray);
+    console.log(priceArray);
+}
+
+calculateButton.addEventListener('click',function(){
+    for(let i=0; i<numberArray.length; i++){
+        totalCost += (numberArray[i] * priceArray[i]);
+        }
+    totalPirceArea.innerHTML = totalCost;
+});
+
+
+
+
+
+
+
+
+
