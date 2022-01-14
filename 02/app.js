8dbc5ae0ab2d26fd3dd8561762c70b5c8fbc72f3
Ewelina <ewelina@ekopacz.pl>
2022-01-14 18:42:01 +0100

    added better solution

--- 02/app.js
+++ 02/app.js
@@ -1,25 +1,47 @@
 const inputElList = document.getElementsByClassName('cart__quantity');
 const numberItem = [ ...inputElList];
+console.log(numberItem);
 const calculateButton = document.querySelector('.cart__btn-calculate');
 const totalPirceArea = document.querySelector('.cart__total-price');
 
-let numberArray = [];
-let priceArray = [];
-let totalCost = 0;
+calculateButton.addEventListener('click', () => {
+    let costProduct = 0;
+    let totalCost = [];
 
-numberItem.forEach(item => item.addEventListener('change',getNumberAndPrice));
+    numberItem.forEach((item) => {
+        const {dataset:{price:itemPrice},value:itemNumber} = item;
+        costProduct = parseInt(itemPrice) * parseInt(itemNumber);
+        totalCost.push(costProduct);
+    });
 
-function getNumberAndPrice(e){
-    numberArray.push(e.target.value);
-    priceArray.push(e.target.dataset.price);
-}
+    orderCost = totalCost.reduce((acc,item) => {
+        return acc + item;
+    });
 
-calculateButton.addEventListener('click',function(){
-    for(let i=0; i<numberArray.length; i++){
-        totalCost += (numberArray[i] * priceArray[i]);
-        }
-    totalPirceArea.innerHTML = totalCost;
+    totalPirceArea.innerHTML = orderCost;
 });
+
+
+
+// WCZESNIEJSZE ROZWIĄZANIE
+
+// let numberArray = [];
+// let priceArray = [];
+// let totalCost = 0;
+
+// numberItem.forEach(item => item.addEventListener('change',getNumberAndPrice));
+
+// function getNumberAndPrice(e){
+//     numberArray.push(e.target.value);
+//     priceArray.push(e.target.dataset.price);
+// }
+
+// calculateButton.addEventListener('click',function(){
+//     for(let i=0; i<numberArray.length; i++){
+//         totalCost += (numberArray[i] * priceArray[i]);
+//         }
+//     totalPirceArea.innerHTML = totalCost;
+// });
 
 
 
