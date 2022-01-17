5b1eb93acd5200df7fbd4e322a4a297444657803
Ewelina <ewelina@ekopacz.pl>
2022-01-17 07:57:34 +0100

    upgrade

--- 04/Admin.js
+++ 04/Admin.js
@@ -1,11 +1,7 @@
 import User from  './User.js';
 
 export default class Admin extends User {
-    constructor( {login = '', password = ''} ){
-        super(login, password);
-        this.login = login;
-        this.password = password;
-    }
+
     isPasswordCorrect() {
         if(this.password.length >= 10) {
             return true;
