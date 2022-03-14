7dfd1d62f9cbce6566d4bfb6759632776a131682
Max <maciejdobisz@gmail.com>
2022-03-14 20:04:19 +0000

    practice-js-es2015plus

--- 04/admin.js
+++ 04/admin.js
@@ -1,10 +1,6 @@
 import User from './User.js';
 
-export default class Admin extends User {
-    constructor(login, password) {
-        super(login, password)
-    }
-    
+export default class Admin extends User {   
     isPasswordCorrect() {
         if(this.password.length >= 10) {
             return true;
