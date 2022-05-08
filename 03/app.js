ca07346d37aa37a0b7b475b2990e80980f1bf503
Filip <43039909+Urfisior@users.noreply.github.com>
2022-05-08 21:28:45 +0100

    update convertToBytes func at task 03

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,14 @@
 import { files } from './data.js';
 
+const unitConverter = {
+    'B': 1,
+    'KB': Math.pow(1024, 1),
+    'MB': Math.pow(1024, 2),
+    'GB': Math.pow(1024, 3)
+};
+
+
+
 const getTotalUnitSize = () => {
     let totalUnit = 0
     files.forEach(({ size: { length, unit = 'B' } }) => {
@@ -9,18 +18,20 @@ const getTotalUnitSize = () => {
 };
 
 const convertToBytes = (value, unit) => {
-    if (unit === 'B') {
-        return value;
-    };
-    if (unit === 'KB') {
-        return value * Math.pow(1024, 1);
-    };
-    if (unit === 'MB') {
-        return value * Math.pow(1024, 2);
-    };
-    if (unit === 'GB') {
-        return value * Math.pow(1024, 3);
-    };
+    return unitConverter[unit] * value
+    // if (unit === 'B') {
+    //     return value;
+    // };
+    // if (unit === 'KB') {
+    //     return value * Math.pow(1024, 1);
+    // };
+    // if (unit === 'MB') {
+    //     return value * Math.pow(1024, 2);
+    // };
+    // if (unit === 'GB') {
+    //     return value * Math.pow(1024, 3);
+    // };
 };
+// console.log(convertToBytes());
 console.log(getTotalUnitSize());
 
