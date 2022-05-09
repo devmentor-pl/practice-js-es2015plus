9357577dce6349b42c7aaf9bc25a7548fa19178a
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-05-09 22:58:05 +0200

    Add changes to exercise 03

--- 03/app.js
+++ 03/app.js
@@ -1,7 +1,47 @@
 
-//import {files} from './data.js';
-//xhttp.open("GET", "https://twoja-domena.pl/download.php", true);
-//const getFiles = require('.03/data.js');
+import files from './data.js';
 
-console.log('kot');
+const [a, b, c, d] = files;
+console.log(files);
+
+const transferToKB = () => {
+    const lengthArray = [];
+    files.forEach( item => {
+    const {
+        name: name,
+        size: {
+            length: lengthValue,
+            unit: unitKind ='B',
+            },
+    } = item;
+    let itemLengthValue = 0;
+    if(unitKind === 'B') {
+        itemLengthValue = lengthValue/1024;
+    }
+    else if(unitKind === 'KB'){
+        itemLengthValue = lengthValue;
+    }
+    else if(unitKind === 'MB'){
+        itemLengthValue = lengthValue*1024;
+    }
+    else if (unitKind === 'GB'){
+        itemLengthValue = lengthValue*1024*1024;
+    }
+    lengthArray.push(itemLengthValue);
+    });
+    console.log(lengthArray);
+    return lengthArray;
+}
+
+const sum = () => {
+    let lengthArray = transferToKB();
+    console.log(lengthArray);
+    const [a, b, c, d] = lengthArray;
+    let sum = a + b + c + d;
+    console.log(sum + ' KB');
+}
+
+sum();
+
+console.log('pies2');
 
--- 03/data.js
+++ 03/data.js
@@ -1,5 +1,4 @@
-//export const files = [
-    const files = [
+ const files = [
     {
         'name': 'img1.jpg',
         'size': {
@@ -29,46 +28,4 @@
     }
 ];
 
-//module.exports = files;
-
-const [a, b, c, d] = files;
-console.log(files);
-
-const changeElementUnit = () => {
-    const lengthArray = [];
-    files.forEach( item => {
-    const {
-        name: name,
-        size: {
-            length: lengthValue,
-            unit: unitKind,
-            },
-    } = item;
-    let itemLengthValue = 0;
-    if(unitKind === undefined) {
-        itemLengthValue = lengthValue/1024;
-    }
-    else if(unitKind === 'KB'){
-        itemLengthValue = lengthValue;
-    }
-    else if(unitKind === 'MB'){
-        itemLengthValue = lengthValue*1024;
-    }
-    else if (unitKind === 'GB'){
-        itemLengthValue = lengthValue*1024*1024;
-    }
-    lengthArray.push(itemLengthValue);
-    });
-    console.log(lengthArray);
-    return lengthArray;
-}
-
-const sum = () => {
-    let lengthArray = changeElementUnit();
-    console.log(lengthArray);
-    const [a, b, c, d ] = lengthArray;
-    let sum = a + b + c + d;
-    console.log(sum);
-}
-
-sum();
+export default files;
