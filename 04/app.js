5f82c71f314ae47e24a4d8186886d230532d1c42
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-05-09 12:58:13 +0200

    task 4

--- 04/app.js
+++ 04/app.js
@@ -5,3 +5,19 @@ const adminData = { login: 'admin@devmentor.pl', password: '1234567890' }
 
 const user = new User( userData );
 user.register();
+
+class Admin extends User{
+    isPasswordCorrect() {
+        if(this.password.length >= 10) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+const admin1 = new Admin( userData );
+admin1.register();
+
+const admin2 = new Admin( adminData );
+admin2.register(); 
