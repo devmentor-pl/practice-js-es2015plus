cd952a6cef232e41b1a3b18e581d577850bbe945
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-05-09 13:54:04 +0200

    task 5

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,4 @@
-import Programmer from './classes/Programmer';
+import Programmer from './classes/Programmer.js';
 
 const features = {
     skills: 10, 
@@ -7,10 +7,25 @@ const features = {
 }
 
 const task = {
-    difficult: 1,
-    size: 1,
+    difficult: 2,
+    size: 2,
 }
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
-console.log(time);
+// const programmer = new Programmer( features );
+// const time = programmer.getApproximateTimeTaskDoneInHours( task );
+
+
+import Junior from './classes/Junior.js';
+import Mid from './classes/Mid.js';
+import Senior from './classes/Senior.js';
+
+const junior = new Junior()
+const mid = new Mid()
+const senior = new Senior()
+
+const timeJunior = junior.getApproximateTimeTaskDoneInHours( task )
+const timeMid = mid.getApproximateTimeTaskDoneInHours( task )
+const timeSenior = senior.getApproximateTimeTaskDoneInHours( task )
+
+const timeForTask = `Junior potrzebuje na wykonanie zadania: ${timeJunior} godzin, mid potrzebuje: ${timeMid} godzin, a senior: ${timeSenior} godzin.`
+console.log(timeForTask)
---
+++ 05/classes/Senior.js
@@ -0,0 +1,19 @@
+import Programmer from './Programmer.js';
+
+export default class Senior extends Programmer{
+    getMinMaxSkills() {
+        return [50, 90];
+    }
+
+    getMinMaxExperience() {
+        return [60, 90];
+    }
+
+    getMinMaxWilligness() {
+        return [40, 60];
+    }
+
+}
+
+const senior = new Senior()
+
