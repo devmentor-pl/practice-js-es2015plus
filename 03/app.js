78355d863fda10daea93759fd12862c2a90dca3e
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-05-10 22:54:22 +0200

    Divide code in exercise 03 into smaller functions

--- 03/app.js
+++ 03/app.js
@@ -1,47 +1,48 @@
 
 import files from './data.js';
 
-const [a, b, c, d] = files;
-console.log(files);
 
-const transferToKB = () => {
-    const lengthArray = [];
-    files.forEach( item => {
+const calculateElKBSize = (element) => {
     const {
         name: name,
         size: {
             length: lengthValue,
             unit: unitKind ='B',
             },
-    } = item;
+    } = element;
+    return convertToKB(lengthValue, unitKind);
+}
+
+const convertToKB = (lengthValue, unitKind) => {
     let itemLengthValue = 0;
-    if(unitKind === 'B') {
-        itemLengthValue = lengthValue/1024;
+    switch(itemLengthValue) {
+        case unitKind === 'B':
+            itemLengthValue = lengthValue/1024;
+            break;
+        case unitKind === 'KB':
+            itemLengthValue = lengthValue;
+            break;
+        case unitKind === 'MB':
+            itemLengthValue = lengthValue*1024;
+            break;
+        case unitKind === 'GB':
+            itemLengthValue = lengthValue*1024*1024;
+            break;
+        default: 
+            console.log('There is a problem with converting to KB');
     }
-    else if(unitKind === 'KB'){
-        itemLengthValue = lengthValue;
-    }
-    else if(unitKind === 'MB'){
-        itemLengthValue = lengthValue*1024;
-    }
-    else if (unitKind === 'GB'){
-        itemLengthValue = lengthValue*1024*1024;
-    }
-    lengthArray.push(itemLengthValue);
-    });
-    console.log(lengthArray);
-    return lengthArray;
+    return itemLengthValue;
 }
 
 const sum = () => {
-    let lengthArray = transferToKB();
-    console.log(lengthArray);
-    const [a, b, c, d] = lengthArray;
-    let sum = a + b + c + d;
+    let sum = 0;
+    files.forEach( item => {
+        sum+=calculateElKBSize(item);
+    });
     console.log(sum + ' KB');
 }
 
 sum();
 
-console.log('pies2');
+
 
