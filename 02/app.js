be1e454a98c64ba5295c7edf951d47de8a5e3066
Andrzej <andis_as@wp.pl>
2022-05-13 20:01:54 +0200

    zad 2 - done

--- 02/app.js
+++ 02/app.js
@@ -1,21 +1,16 @@
 console.log('zad 2')
 
 const cart = document.getElementsByClassName('cart__quantity')
-console.log(cart)
 const button = document.querySelector('.cart__btn-calculate')
 const total = document.querySelector('.cart__total-price')
 
 const newCart = [...cart]
-console.log(newCart)
 
-const calculate = (e) => {
-    console.log(e)
+const calculate = () => {
     let totalPrice = 0
     newCart.forEach(cart => {
         const price = cart.dataset.price
-        console.log(price)
         const quantity = cart.value
-        console.log(quantity)
         totalPrice += quantity * price
     })
     total.innerText = totalPrice
