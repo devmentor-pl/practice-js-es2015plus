e81e17f6f19e5bd50d30aca3a6ed4090191d4e20
Camila Montes Celinski <camicelinski@gmail.com>
2022-05-29 11:01:21 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,25 @@
 import {files} from './data.js';
 
+function convertToBytes(length, unit) {
+    if(unit === 'B') {
+        return length
+    } else if(unit === 'KB') {
+        return length * 1024
+    } else if(unit === 'MB') {
+        return length * 1024 * 1024
+    } else if(unit === 'GB') {
+        return length * 1024 * 1024 * 1024
+    }        
+}
+
+const getTotalSize = () => {
+    let size = 0
+
+    files.forEach(({size: {length, unit = 'B'}}) => {
+        size += convertToBytes(length, unit)
+    })
+
+    return size
+}
+
+console.log(`Suma przestrzeni zajmowanej przez wszystkie pliki = ${getTotalSize()} B.`)
