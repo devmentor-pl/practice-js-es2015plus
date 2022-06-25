f5430e7ccf5e701c28815e0f0367e8cf86e1a4f3
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-06-25 18:03:42 +0200

    finish 05 task

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -13,4 +16,22 @@ const task = {
 
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
-console.log(time);
+// console.log(time);
+
+//junior
+
+const junior = new Junior();
+const timeJunior = junior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Junior time: ${timeJunior}`);
+
+//mid 
+
+const mid = new Mid();
+const timeMid = mid.getApproximateTimeTaskDoneInHours(task);
+console.log(`Mid time: ${timeMid}`);
+
+//senior
+
+const senior = new Senior();
+const timeSenior = senior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Senior time: ${timeSenior}`);
