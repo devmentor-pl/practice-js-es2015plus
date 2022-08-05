0bb3a389956dabf61d1db15fc3bb0b2e0c035d6e
Marta Stelmach <marta.ms1961@gmail.com>
2022-08-05 11:14:08 +0200

    Finish task-4

---
+++ 04/Admin.js
@@ -0,0 +1,10 @@
+import User from "./User";
+
+export default class Admin extends User {
+    isPasswordCorrect() {
+        if (this.password.length >= 10) {
+            return true;
+        }
+        return false;
+    };
+};
