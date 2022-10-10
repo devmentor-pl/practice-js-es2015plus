d720d38a7f65ac9ff4164c9986809bf68ff60fb9
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-10-10 15:02:47 +0200

    Add classes Junior, Mid, Senior

---
+++ 05/classes/Junior.js
@@ -0,0 +1,19 @@
+import Programmer from './Programmer';
+
+export default class Junior extends Programmer {
+    constructor({skills = null, experience = null, willingness = null} = {}){
+        super({skills, experience, willingness})
+    }
+
+    getMinMaxSkills() {
+        return [0, 30];
+    }
+
+    getMinMaxExperience() {
+        return [0, 30];
+    }
+
+    getMinMaxWilligness() {
+        return [30, 70]
+    }
+}
