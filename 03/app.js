bc19265ef232e653c8f68cc597b4762f877248e3
Jacek Zielinski <gandi34@wp.pl>
2022-11-06 15:06:51 +0100

    js es 2015 plus

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,21 @@
 import {files} from './data.js';
 
+function convertLarge(lenght ,unit = 'B'){
+    if(unit === 'B') {
+        return lenght;
+    } else if (unit ==='KB') {
+        return lenght * 1024;
+    }else if(unit === 'MB') {
+        return lenght *1024 * 1024
+    }else if(unit ==='GB') {
+        return lenght *1024 * 1024 * 1024;
+    }
+    }
+    
+    let large = 0;
+    files.forEach (i => {
+        large += convertLarge(i.size.length,i.size.unit);
+    });
+
+    console.log(large);
+
