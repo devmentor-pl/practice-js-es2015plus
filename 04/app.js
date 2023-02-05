d8b87469cc5309a0384aa368f9e4fa70e7ab52fd
Martyna Giee <martyna.giee@gmail.com>
2023-02-05 17:40:53 +0100

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,10 @@ const adminData = { login: 'admin@devmentor.pl', password: '1234567890' }
 
 const user = new User(userData);
 user.register();
+
+const admin1 = new Admin(userData);
+admin1.register(); // error
+
+const admin2 = new Admin(adminData);
+admin2.register(); // done
+
