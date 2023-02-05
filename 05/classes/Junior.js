6ca93ebafb5f0ef68094875a8048c8c9c0111021
Martyna Giee <martyna.giee@gmail.com>
2023-02-05 18:14:54 +0100

    task 5 done

---
+++ 05/classes/Junior.js
@@ -0,0 +1,20 @@
+import Programmer from './Programmer';
+
+
+class Junior extends Programmer {
+
+    getMinMaxSkills() {
+        return [0, 30];
+    }
+
+    getMinMaxExperience() {
+        return [0, 30];
+    }
+
+    getMinMaxWilligness() {
+        return [30, 70]
+    }
+
+}
+
+export default Junior
