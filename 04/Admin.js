948b32b998000b63c49e1bcbc1f72632f4d429f1
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-03-09 17:22:12 +0100

    task-04 done

---
+++ 04/Admin.js
@@ -0,0 +1,17 @@
+import User from './User.js'
+
+export default class Admin extends User {
+	constructor({ login, password }) {
+		super(login, password)
+		this.login = login
+		this.password = password
+	}
+
+	isPasswordCorrect() {
+		if (this.password.length >= 10) {
+			return true
+		}
+
+		return false
+	}
+}
