4e639d3376ac64d0a663eaf709f7c5a505da6cef
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-03-09 16:24:26 +0100

    task-03 done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,33 @@
-import {files} from './data.js';
+// import { files } from './data.js'
+const files = require('./data.js')
 
+const calcSizeFiles = (arr) => {
+	let sum = 0
+
+	arr.forEach((item) => {
+		const {
+			size: { length },
+			size: { unit },
+		} = item
+
+		sum += convertSizeToByte(length, unit)
+	})
+	// console.log(sum)
+	return sum.toFixed(2)
+}
+
+const convertSizeToByte = (length, unit = 'B') => {
+	if (unit === 'GB') {
+		return (fileSize = length * 1024 * 1024 * 1024)
+	} else if (unit === 'MB') {
+		return (fileSize = length * 1024 * 1024)
+	} else if (unit === 'KB') {
+		return (fileSize = length * 1024)
+	} else if (unit === 'B') {
+		return (fileSize = length)
+	}
+}
+
+console.log(
+	`Wielkość: ${calcSizeFiles(files)} B, ilość plików: ${files.length}.`
+)
