e9c32913be2f5d979d021309d1fbbe91fe663692
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-03-13 11:24:18 +0100

    update task-03

--- 03/app.js
+++ 03/app.js
@@ -29,3 +29,28 @@ const convertSizeToByte = (length, unit = 'B') => {
 console.log(
 	`Wielkość: ${calcSizeFiles(files)} B, ilość plików: ${files.length}.`
 )
+
+// rozwiązanie Mateusza B
+function convertToBytes(length, unit) {
+	const data = {
+		'B': 1,
+		'KB': 1024,
+		'MB': 1024 * 1024,
+		'GB': 1024 * 1024 * 1024,
+	}
+
+	if (typeof data[unit] === 'undefined') {
+		console.error('Nie mam takiej jednostki')
+	}
+	return data[unit] * length
+}
+
+
+const getTotalSize = () => {
+	let size = 0
+	files.forEach(({ size: { length, unit = 'B' } }) => {
+		size += convertToBytes(length, unit)
+	})
+	return size
+}
+console.log(`${getTotalSize()} rozwiązanie Mateusza z YT`);
