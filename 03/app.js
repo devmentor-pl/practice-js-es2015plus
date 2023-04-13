b4bafa78897d90f55da6a7b6f0a342aa5090ef99
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-04-13 18:33:23 +0200

    finish task3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,40 @@
-import {files} from './data.js';
+import {files as filesArray} from './data.js';
+
+let sum = 0;
+
+const getFileSizes = (array) => {
+    totalizeFilesSizes(array);
+    renderData(array, sum)
+}
+
+const renderData = (sourceArray) => {
+    sourceArray.forEach(fileObject=> {
+        const {name, size:{length,unit,unitExponent}}=  fileObject;
+        const magnitude = Math.pow(1024,unitExponent);
+
+        if(unitExponent && !(sum > magnitude)){
+            const roundedSize = (sum / magnitude).toFixed(2);
+            console.log(`${roundedSize} ${unit}`);
+        };
+    })
+}
+
+const totalizeFilesSizes = (arrayOfFiles) => {
+    arrayOfFiles.forEach(fileObject=> {
+        const {name, size:{length,unit,unitExponent}}=  fileObject;
+        addSingleSize(length,unitExponent);
+    })
+}
+
+const addSingleSize = (length, unitExponent=0) => {
+    const magnitude = Math.pow(1024,unitExponent);
+    sum+=(magnitude*length);
+}
+
+
+getFileSizes(filesArray);
+
+
+
+
 
