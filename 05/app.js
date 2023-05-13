7aade7f5c9eeed202944c8e48d7f4d2c14916f9b
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-05-13 11:59:20 +0200

    Added task 5

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -14,3 +17,15 @@ const task = {
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+
+const junior = new Junior();
+const time2 = junior.getApproximateTimeTaskDoneInHours( task );
+console.log(time2);
+
+const mid = new Mid();
+const time3 = mid.getApproximateTimeTaskDoneInHours( task );
+console.log(time3);
+
+const senior = new Senior();
+const time4 = senior.getApproximateTimeTaskDoneInHours( task );
+console.log(time4);
