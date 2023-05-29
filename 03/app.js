cdc6a4a0dde63e360dda971c0c12e26ebda957fa
Jakub Kasperkowicz <jakub.kasperkowicz@gmail.com>
2023-05-29 23:51:24 +0200

    Finished 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,38 @@
-import {files} from './data.js';
+import { files } from './data.js';
 
+function convertToBytes(length, unit = 'B') {
+
+const data = {
+    'B': 1,
+    'KB': 1024,
+    'MB': 1024 * 1024,
+    'GB': 1024 * 1024 * 1024,
+}
+
+if(typeof data[unit] === 'undefined'){
+    console.error('Nie ma takiej jednostki')
+}
+return data[unit] * length
+
+
+    // if (unit === 'B') {
+    //     return length
+    // } else if (unit === 'KB') {
+    //     return length * 1024
+    // } else if (unit === 'MB') {
+    //     return length * 1024 * 1024
+    // } else if (unit = 'GB') {
+    //     return length * 1024 * 1024 * 1024
+    // }
+}
+
+const getTotalSize = () => {
+    let size = 0
+    files.forEach(({size: {length, unit}}) => {
+        size += convertToBytes(length, unit)
+    })
+
+    return size
+}
+
+console.log(getTotalSize())
