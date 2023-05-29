6a4bfefe39bfd30e3c481c33078fd1dabca8cb80
Marcin Soltysik <profittax@gmail.com>
2023-05-29 20:54:55 +0200

    Done 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,23 @@
-import {files} from './data.js';
+import { files } from './data.js';
 
+console.log(files);
+
+function convertToBytes(length, unit = 'B') {
+  if (unit === 'B') {
+    return length;
+  } else if (unit === 'KB') {
+    return length * 1024;
+  } else if (unit === 'MB') {
+    return length * 1024 * 1024;
+  } else if (unit === 'GB') {
+    return length * 1024 * 1024 * 1024;
+  }
+}
+
+let size = 0;
+files.forEach((item) => {
+  // console.log(item.size.length);
+  size = size + convertToBytes(item.size.length, item.size.unit);
+});
+
+console.log(size);
