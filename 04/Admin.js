0784e4632b5d474643f9342e42dd1d61aee9dd4f
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-02 13:11:07 +0200

    delete constructor

--- 04/Admin.js
+++ 04/Admin.js
@@ -1,10 +1,6 @@
 import User from "./User.js";
 
 export default class Admin extends User {
-  constructor(adminData) {
-    super(adminData);
-  }
-
   isPasswordCorrect() {
     if (this.password.length >= 10) {
       return true;
