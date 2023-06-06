d0187b0be25a23c6fec1cb3808799c85f6032c70
mike161pl <mike161pl@gmail.com>
2023-06-06 08:41:24 +0200

    task finished

---
+++ 04/Admin.js
@@ -0,0 +1,40 @@
+export default class Admin {
+    constructor( {login = '', password = ''} ) {
+        this.login = login;
+        this.password = password;
+    }
+
+    register() {
+        if(this.isValid()) {
+            console.log('done');
+            return true;
+        }
+
+        console.log('error');
+        return false;
+    }
+
+    isValid() {
+        if(this.isLoginCorrect() && this.isPasswordCorrect()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    isLoginCorrect() {
+        if(this.login.includes('@')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    isPasswordCorrect() {
+        if(this.password.length > 10) {
+            return true;
+        }
+
+        return false;
+    }
+}
