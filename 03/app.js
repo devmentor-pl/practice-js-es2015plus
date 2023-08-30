d5287bc3ea9ae584ac53d14a0fdc4371aca0eb65
Michal Nowakowski <nowakowski.m1995@gmail.com>
2023-08-30 18:53:11 +0200

    ex3 done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,39 @@
-import {files} from './data.js';
+import { files } from "./data.js";
 
+// 1KB = 1024B, 1MB = 1024KB
+const convertSizeToMB = (type, size) => {
+	let sizeInMB = 0;
+	switch (type) {
+		case "B":
+			sizeInMB = size / Math.pow(1024, 2);
+			break;
+		case "KB":
+			sizeInMB = size / 1024;
+			break;
+		case "GB":
+			sizeInMB = size * 1024;
+			break;
+		default:
+			sizeInMB = parseInt(size);
+			break;
+	}
+
+	return sizeInMB;
+};
+
+const sumFilesSize = (files) => {
+	let sum = 0;
+	const filesCopy = files.slice();
+
+	sum = filesCopy.reduce((acc, file) => {
+		const { length } = file.size;
+		const { unit = "B" } = file.size;
+
+		return acc + convertSizeToMB(unit, length);
+	}, 0);
+	sum = sum.toFixed(2);
+	console.log(sum);
+	return sum;
+};
+
+sumFilesSize(files);
