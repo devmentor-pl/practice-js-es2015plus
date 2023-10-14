70ee9a05b68adf0af1021dd2f74c12e626d0e21a
Maciej Necka <macieknecka@gmail.com>
2023-10-14 11:57:34 +0200

    Task done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,26 @@
-import {files} from './data.js';
+import { files } from './data.js';
 
+const convertSizeToBytes = (size, unit = 'B') => {
+  const units = {
+    B: 1,
+    KB: 1024,
+    MB: 1024 * 1024,
+    GB: 1024 * 1024 * 1024,
+  };
+  return size * units[unit];
+};
+
+const calcTotalSize = (files) => {
+  let totalSize = 0;
+  for (const file of files) {
+    const { size } = file;
+    const { length, unit = 'B' } = size;
+    const sizeInBytes = convertSizeToBytes(length, unit);
+    totalSize += sizeInBytes;
+  }
+  return totalSize;
+};
+
+console.log(
+  `Suma wielkości wszystkich plików wynosi: ${calcTotalSize(files)} B`
+);
