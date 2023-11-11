fb39481b82f6a4c6b889f52e79261c4d5ed1292d
Piotr Marczykowski <piotr.marczykowski94@gmail.com>
2023-11-11 13:08:50 +0100

    init

--- 03/app.js
+++ 03/app.js
@@ -1,32 +1,5 @@
-// import files from './data.js';
-const files = [{
-        'name': 'img1.jpg',
-        'size': {
-            length: 215,
-        }
-    },
-    {
-        'name': 'img2.jpg',
-        'size': {
-            length: 145,
-            unit: 'KB',
-        }
-    },
-    {
-        'name': 'img3.jpg',
-        'size': {
-            length: 14,
-            unit: 'MB',
-        }
-    },
-    {
-        'name': 'img4.jpg',
-        'size': {
-            length: 0.9,
-            unit: 'GB',
-        }
-    }
-];
+import files from './data.js';
+
 // zamiana do wspólnej jednostki
 const convertIntoOneUnits = (size, unit = 'B') => size * ({'B': 1, 'KB': 1024, 'MB': 1024 ** 2, 'GB':  1024 ** 3}[unit])
 
