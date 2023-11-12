86f489d2f3bb5a8275c4522666ad195628f8bcea
Michal Gawlik <gawlik.zator@o2.pl>
2023-11-12 17:19:54 +0100

    practice-js-es2015plus-complete

--- 05/app.js
+++ 05/app.js
@@ -1,16 +1,31 @@
-import Programmer from './classes/Programmer';
+import Programmer from './classes/Programmer'
+import Junior from './classes/Junior'
+import Mid from './classes/Mid'
+import Senior from './classes/Senior'
 
 const features = {
-    skills: 10, 
-    experience: 10, 
-    willingness: 10,
+	skills: 10,
+	experience: 10,
+	willingness: 10,
 }
 
 const task = {
-    difficult: 1,
-    size: 1,
+	difficult: 1,
+	size: 1,
 }
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
-console.log(time);
+const programmer = new Programmer(features)
+const time = programmer.getApproximateTimeTaskDoneInHours(task)
+console.log(time)
+
+const juniorProgrammer = new Junior()
+const juniorProgrammerTime = juniorProgrammer.getApproximateTimeTaskDoneInHours(task)
+console.log(juniorProgrammerTime)
+
+const midProgrammer = new Mid()
+const midProgrammerTime = midProgrammer.getApproximateTimeTaskDoneInHours(task)
+console.log(midProgrammerTime)
+
+const seniorProgrammer = new Senior()
+const seniorProgrammerTime = seniorProgrammer.getApproximateTimeTaskDoneInHours(task)
+console.log(seniorProgrammerTime)
