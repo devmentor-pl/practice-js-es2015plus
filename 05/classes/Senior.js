025532b81a61494df7892c70315f5d9410942f1c
Kasia <k.kasia912@gmail.com>
2023-12-18 15:42:04 +0100

    complete task 05

---
+++ 05/classes/Senior.js
@@ -0,0 +1,17 @@
+import Programmer from './Programmer';
+export default class Senior extends Programmer {
+  constructor({skills = null, experience = null, willingness = null} = {}) {
+    super({skills, experience, willingness});
+  }
+  getMinMaxSkills() {
+    return [50, 90];
+  }
+
+  getMinMaxExperience() {
+    return [60, 90];
+  }
+
+  getMinMaxWilligness() {
+    return [40, 60];
+  }
+}
