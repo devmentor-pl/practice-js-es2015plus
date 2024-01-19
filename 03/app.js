f5f7b011e043c219431b79cd758bdd505883e025
Dawidtk <dawidtk@gmail.com>
2024-01-19 15:38:36 +0100

    convertUnits

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,15 @@
 import {files} from './data.js';
 
+import { files } from "./data.js";
+
+const conUnits = (unit, length) => {
+	if (unit === "KB") {
+		return length * 1024;
+	} else if (unit === "MG") {
+		return length * Math.pow(1024, 2);
+	} else if (unit === "GB") {
+		return length * Math.pow(1024, 3);
+	} else {
+		return length;
+	}
+};
