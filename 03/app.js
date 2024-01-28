3aeeed747c9c26e815710218d18edd8bd41dc32b
Kamil Lewandowski <kamlew555@gmail.com>
2024-01-28 18:39:18 +0100

    task done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,22 @@
 import {files} from './data.js';
 
+// Funkcja do obliczania sumy przestrzeni zajmowanej przez pliki
+const calculateTotalSpace = (files) => {
+  // Funkcja do przeliczania rozmiarów na bajty
+  const convertToBytes = (size, unit = 'B') => {
+    const unitMultiplier = { 'B': 1, 'KB': 1024, 'MB': 1024 * 1024, 'GB': 1024 * 1024 * 1024 };
+    return size * unitMultiplier[unit];
+  };
+
+  // Funkcja pomocnicza do sumowania rozmiarów
+  const sumSizes = (total, file) => total + convertToBytes(file.size.length, file.size.unit);
+
+  // Obliczam sumę
+  const totalSpace = files.reduce(sumSizes, 0);
+
+  return totalSpace;
+};
+
+
+const totalSpace = calculateTotalSpace(files);
+console.log(`Total space occupied by all files: ${totalSpace} B`);
