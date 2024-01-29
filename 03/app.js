146dee95dd242cd59a89f1fe56b60086f0eb58df
Kamil Lewandowski <kamlew555@gmail.com>
2024-01-29 21:07:13 +0100

    modified task

--- 03/app.js
+++ 03/app.js
@@ -4,9 +4,20 @@ import {files} from './data.js';
 const calculateTotalSpace = (files) => {
   // Funkcja do przeliczania rozmiarów na bajty
   const convertToBytes = (size, unit = 'B') => {
-    const unitMultiplier = { 'B': 1, 'KB': 1024, 'MB': 1024 * 1024, 'GB': 1024 * 1024 * 1024 };
-    return size * unitMultiplier[unit];
-  };
+		const unitMultiplier = {
+			B: 1,
+			KB: 1024,
+			MB: 1024 * 1024,
+			GB: 1024 * 1024 * 1024,
+		};
+
+		// Sprawdzenie czy jednostka istnieje w spisie, jeśli nie, to zwracamy 0
+		if (!unitMultiplier[unit]) {
+			console.error(`Unknown unit: ${unit}. Unable to convert size.`);
+			return 0;
+		}
+		return size * unitMultiplier[unit];
+	};
 
   // Funkcja pomocnicza do sumowania rozmiarów
   const sumSizes = (total, file) => total + convertToBytes(file.size.length, file.size.unit);
