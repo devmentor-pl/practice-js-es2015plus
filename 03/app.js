3326066614ed6bac010d7415dc179556461ca555
Robert Staniszewski <robstani.tech@gmail.com>
2024-02-02 18:03:20 +0100

    Create function to covert units to bytes

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,22 @@
 import {files} from './data.js';
+const data = files;
+
+
+const convertToBytes = ({ size, unit = 'B'}) => {
+    const sizeInBytes = parseInt(size, 10);
+
+    // Konwersja jednostek na bajty
+    switch (unit.toUpperCase()) {
+        case 'KB':
+            return sizeInBytes * 1024;
+        case 'MB':
+            return sizeInBytes * 1024 * 1024;
+        case 'GB':
+            return sizeInBytes * 1024 * 1024 * 1024;
+        default:
+            return sizeInBytes;
+    }
+};
+
+
 
