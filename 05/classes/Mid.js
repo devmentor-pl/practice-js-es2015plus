b16ab5435ca90c89d91a98a0d396a0a47a2c02b6
Robert Staniszewski <robstani.tech@gmail.com>
2024-02-12 19:46:51 +0100

    Inherit 'Junior', 'Mid', and 'Senior' class from 'Programmer'

--- 05/classes/Mid.js
+++ 05/classes/Mid.js
@@ -1,31 +1,12 @@
-export default class Mid {
+import Programmer from './Programmer';
+
+export default class Mid extends Programmer {
     constructor( {skills = null, experience = null, willingness = null} = {} ) {
+        super();
+        
         this.skills = this.isNumber(skills) ? skills : this.getSkillsRandomNumber();
         this.experience = this.isNumber(experience) ? experience : this.getExperienceRandomNumber();
         this.willingness = this.isNumber(willingness) ? willingness : this.getWillingnessRandomNumber();
-    }
-
-    isNumber(value) {
-        if(
-            typeof value === 'number'
-            && value === value // NaN === NaN => false
-        ) {
-            return true;
-        }
-
-        return false;
-    }
-
-    getSkillsRandomNumber() {
-        return this.getRandomNumber(...this.getMinMaxSkills());
-    }
-
-    getExperienceRandomNumber() {
-        return this.getRandomNumber(...this.getMinMaxExperience())
-    }
-
-    getWillingnessRandomNumber() {
-        return this.getRandomNumber(...this.getMinMaxWilligness())
     }
 
     getMinMaxSkills() {
@@ -38,28 +19,5 @@ export default class Mid {
 
     getMinMaxWilligness() {
         return [50, 90]
-    }
-
-    getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-
-    getApproximateTimeTaskDoneInHours( {difficult, size} ) {
-        let counter = 0;
-        let time = 0;
-        let rand;        
-
-        const done = 5 * difficult * size;
-        const chance = ( (0.5 * this.skills) + (0.3 * this.experience) + (0.2 * this.willingness)) / 3;
-        while(counter < done && time < 1000) {
-            rand = this.getRandomNumber(0, 100);
-            if(rand <= chance) {
-                counter++;
-            }
-
-            time += 0.25;
-        }
-
-        return time;
     }
 }
