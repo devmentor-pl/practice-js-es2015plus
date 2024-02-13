334ec8edada9782861575147b59d5de134d2b594
Robert Staniszewski <robstani.tech@gmail.com>
2024-02-13 20:43:48 +0100

    Remove constructor from Admin class, as it inherits it from User class

--- 04/Admin.js
+++ 04/Admin.js
@@ -2,10 +2,6 @@ import User from './User.js';
 
 // Rozszerzenie klasy User o klasę Admin
 export default class Admin extends User {
-    constructor({ login = '', password = '' }) {
-        super({ login, password });
-    }
-
     isPasswordCorrect() {
         // Nadpisanie metody aby dostosowac kryteria hasła adminów
         return this.password.length >= 10;
