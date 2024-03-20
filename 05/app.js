625de9b13715e11b47aaa8bcdef15c68e49ae79f
Patryk <patryk.13@o2.pl>
2024-03-20 18:43:16 +0100

    Done 05

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -14,3 +17,15 @@ const task = {
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+
+const junior = new Junior();
+const time_junior = junior.getApproximateTimeTaskDoneInHours( task );
+console.log(`Junior wykona zadanie w ${time_junior} godzin`);
+
+const mid = new Mid();
+const time_mid = mid.getApproximateTimeTaskDoneInHours( task );
+console.log(`Mid wykona zadanie w ${time_mid} godzin`);
+
+const senior = new Senior();
+const time_senior = senior.getApproximateTimeTaskDoneInHours( task );
+console.log(`Senior wykona zadanie w ${time_senior} godzin`);
