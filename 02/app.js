6ba5173a99380589a48df16d9b99a959685b6f26
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-04-03 16:24:02 +0200

    03 done

--- 02/app.js
+++ 02/app.js
@@ -1,49 +1,20 @@
-// KONSULTACJE - BŁĄD W LINII 36  i 48
-// + jak uyć tutaj destrukturyzację? czytając skrypt wydaje mi się ona zrozumiała, ale tutaj mi nie pasuje jej uzycie.
-
-// pobieramy wpisane wartości i ceny //DZIAŁA
-const getInputsValue = event => {
-    event.preventDefault();
-    event.stopPropagation();
-    
-    event.target.setAttribute('value', event.target.value); //1
-    const a = parseInt(event.target.value);
-    // console.log('a=>', a);
-    
-    const b = parseInt(event.target.getAttribute('data-price'));
-    // console.log('b=> ', b); 
-    
-    //pobieramy wartości do tablicy i zwracamy
-    const arr = [a, b]
-    console.log(arr);
-    return arr;
-}
-
+// DZIAŁA
 const cart = document.querySelector('.cart');
 const inputEl = Array.from(cart.getElementsByClassName('cart__quantity')); // array
 
-inputEl.forEach(function(input) {
-    input.addEventListener('change', getInputsValue);
-})
+const totalPrice = cart.querySelector('.cart__total-price');
 
-//obliczamy sumę 
 const button = cart.querySelector('button');
-console.log(button);
 
-const getSum = event => {
-    event.preventDefault();
-    event.stopPropagation();
- // BŁĄD - nic się nie oblicza   
-    getInputsValue = [a + b];
-    sum += [a + b];
-    console.log(sum);
-    return sum;
+const handleClick = event => {
+    let sum = 0;
+    inputEl.forEach(function(input) {
+        const { value, dataset } = input;
+        const { price } = dataset;
+
+        sum += value * price;
+    })
+    totalPrice.innerText = sum;
 }
 
-button.addEventListener('submit', getSum)
-
-// WSTAWIAMY SUMĘ 
-const totalPrice = cart.querySelector('.cart__total-price');
-console.log(totalPrice);
-totalPrice.innerText = getSum; //BłĄD - wstawia całą funkcję, zamiast wyniku
-
+button.addEventListener('click', handleClick)
