0d1c4b9e879deaa40baf4877597a07daf3e3619b
Paweł Suder <pawel.suder94@gmail.com>
2024-05-07 16:08:01 +0200

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,29 @@
-import {files} from './data.js';
+import { files } from './data.js';
 
+function convertToBytes(length, unit) {
+
+    const data = {
+        'B': 1,
+        'KB': 1024,
+        'MB': 1024 * 1024,
+        'GB': 1024 * 1024 * 1024
+
+    }
+
+    if (typeof data[unit] === 'undefined') {
+        console.error('Brak takiej jednostki')
+    }
+    return data[unit] * length
+}
+
+const totalSize = () => {
+    let size = 0
+    files.forEach(({ size: { length, unit = 'B' } }) => {
+        size += convertToBytes(length, unit)
+    })
+
+    return size
+}
+
+
+console.log(totalSize())
