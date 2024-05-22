6916de864fd01241e933680362154813d3698ea6
Jarek Stelmach <j.stelmach@onet.eu>
2024-05-22 21:50:13 +0200

    Finished ex2 in JS ES2015 practice

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Cos');
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("Cos")
+
+	const inputsGroup = document.getElementsByClassName("cart__quantity")
+    const inputsGroupArr = Array.from(inputsGroup)
+    
+	const countBtn = document.querySelector(".cart__btn-calculate")
+    const totalPrice = document.querySelector('.cart__total-price')
+
+	const btnClickFn = () => {
+		let priceSum = 0
+        inputsGroupArr.forEach((el) => {
+
+            const { dataset } = el
+            const { value } = el 
+            priceSum = priceSum + (dataset.price * value)
+        })
+        
+        totalPrice.innerText = priceSum + ' '
+
+	}
+
+	countBtn.addEventListener("click", btnClickFn)
 })
