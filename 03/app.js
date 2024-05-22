e5981d460af03de5032e4ac714941a205139ba5f
Jarek Stelmach <j.stelmach@onet.eu>
2024-05-22 22:18:06 +0200

    Finished ex3 in JS ES2015 practice

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,32 @@
 import {files} from './data.js';
+let totalSize = 0
 
+function changeToBytes(length, unit = 'B') {
+
+    if(unit === 'B') {
+        return length
+    } else if (unit === "KB") {
+        return length * 1024
+    } else if (unit === "MB") {
+        return length * 1024 * 1024
+    } else if (unit === "GB") {
+        return length * 1024 * 1024 * 1024
+    }
+} 
+
+const SumAllSizes = () => {
+    files.forEach(file => {
+        // console.log(file);
+    
+        const { size: {length}} = file
+        // console.log(length);
+        const { size: {unit}} = file
+        // console.log(unit);
+        
+        totalSize = totalSize + (changeToBytes(length, unit))
+    })
+    return totalSize
+}
+
+
+console.log("Suma wszystkich plików:", " ", SumAllSizes(), "B");
