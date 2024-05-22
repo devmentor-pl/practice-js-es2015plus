9ffa63101a70e3451db7d6567c3732a788c0b314
Marcin Wabinski <wabinskim@gmail.com>
2024-05-22 23:59:32 +0200

    Finish task 05

--- 05/app.js
+++ 05/app.js
@@ -1,16 +1,37 @@
-import Programmer from './classes/Programmer';
+import Programmer from "./classes/Programmer";
+import Junior from "./classes/Junior";
+import Mid from "./classes/Mid";
+import Senior from "./classes/Senior";
 
 const features = {
-    skills: 10, 
-    experience: 10, 
-    willingness: 10,
-}
+  skills: 10,
+  experience: 10,
+  willingness: 10,
+};
 
 const task = {
-    difficult: 1,
-    size: 1,
-}
+  difficult: 1,
+  size: 1,
+};
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
+const programmer = new Programmer(features);
+const time = programmer.getApproximateTimeTaskDoneInHours(task);
 console.log(time);
+
+const junior = new Junior();
+const juniorTime = junior.getApproximateTimeTaskDoneInHours(task);
+console.log(
+  `Czas na wykonanie zadania o stopniu trudności ${task.difficult} i wielkości ${task.size} przez juniora wynosi ${juniorTime} godzin.`
+);
+
+const mid = new Mid();
+const midTime = mid.getApproximateTimeTaskDoneInHours(task);
+console.log(
+  `Czas na wykonanie zadania o stopniu trudności ${task.difficult} i wielkości ${task.size} przez mida wynosi ${midTime} godzin.`
+);
+
+const senior = new Senior();
+const seniorTime = senior.getApproximateTimeTaskDoneInHours(task);
+console.log(
+  `Czas na wykonanie zadania o stopniu trudności ${task.difficult} i wielkości ${task.size} przez seniora wynosi ${seniorTime} godzin.`
+);
