f1af86ad81403901922a55118b01fe340ecd8c90
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-05-23 22:45:25 +0200

    add task 3

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,18 @@
 import {files} from './data.js';
+const convert = require('./convert.js');
 
+let sum = 0;
+
+
+files.forEach((el)=> {
+    
+const elementSize = el.size
+
+const convertedToB = convert(elementSize.length, elementSize.unit)
+
+sum += convertedToB
+
+console.log(sum)
+
+})
+ 
