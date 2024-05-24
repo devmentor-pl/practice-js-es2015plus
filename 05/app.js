dcd1501cc98f80c99516b6c3be21d0fc0c2160f3
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-05-24 17:58:04 +0200

    add task 05

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -11,6 +14,20 @@ const task = {
     size: 1,
 }
 
-const programmer = new Programmer( features );
+const programmer = new Programmer( features);
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
+console.log(programmer);
 console.log(time);
+
+ const juniorProgrammer = new Junior (features)
+const timeJunior = juniorProgrammer.getApproximateTimeTaskDoneInHours( task );
+console.log(timeJunior);
+
+const midProgrammer = new Mid ()
+const timeMid = midProgrammer.getApproximateTimeTaskDoneInHours( task );
+console.log(timeMid);
+
+
+const seniorProgrammer = new Senior ()
+const timeSenior = seniorProgrammer.getApproximateTimeTaskDoneInHours( task );
+console.log(timeSenior);
