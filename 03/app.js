401d192d2c2c8770f09f03a54a58c1684c4b840e
MarcinMorgan <morgangzit@gmail.com>
2024-06-01 18:25:17 +0200

    feat: t3 import file data

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,10 @@
 import {files} from './data.js';
 
+const fileDataUpload = elem => {
+    const { size: { length = 0, unit = 'B' }} = elem;
+    return [length, unit];
+}
+
+files.forEach(elem => {
+    console.log(fileDataUpload(elem))
+})
