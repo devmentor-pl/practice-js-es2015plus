9de9168d1747738d825acd8406ed2bd9d0c65c2c
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-06-01 20:58:37 +0200

    Task 02 refactor

--- 02/app.js
+++ 02/app.js
@@ -5,23 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const button = document.querySelector('.cart__btn-calculate')
     button.addEventListener('click', () => {
-        console.log("dupa");
-
         let sum = 0;        
 
         inputs.forEach((element) => {            
-            const {value, dataset: {price}} = element;
-            
+            const {value, dataset: {price}} = element;            
             sum += value * dataset.price;
         })
 
         sum = sum.toFixed(2);
 
-        console.log(sum);
-
         const priceHeader = document.querySelector('.cart__total-price');
         priceHeader.innerText = sum;
-
     })    
 });
 
