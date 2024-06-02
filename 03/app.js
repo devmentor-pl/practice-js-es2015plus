511229fd3e860c865ae711fbb563d02d45c821bd
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-06-02 12:32:48 +0200

    Task 03 finished

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,25 @@
-import {files} from './data.js';
+import { files } from './data.js';
 
+const totalSizeInBytes = () => {
+    const total = files.reduce(
+        (totalSize, fileInfo) => {
+            const { size: { length, unit = 'B' } } = fileInfo;
+            return totalSize + calculateFileSizeInBytes(length, unit);
+        }, 0);
+        return total.toFixed(0);
+}
+
+function calculateFileSizeInBytes(length, unit) {
+    let totalLength = length;
+
+    switch (unit) {
+        case 'GB': totalLength *= 1024;
+        case 'MB': totalLength *= 1024;
+        case 'KB': totalLength *= 1024;
+        default:
+    }
+
+    return totalLength;
+}
+
+console.log('Total size: ' + totalSizeInBytes() + ' Bytes');
