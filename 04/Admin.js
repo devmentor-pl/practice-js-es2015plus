b1e8fc6e84fc23ea6229d93acb16df60f1a7d56c
BartekB94 <wurir1994@gmail.com>
2024-06-25 00:19:50 +0200

    complete

---
+++ 04/Admin.js
@@ -0,0 +1,13 @@
+import User from './User';
+
+export default class Admin extends User {
+    constructor( {login = '', password = ''}) {
+        super({ login, password })
+    }
+    isPasswordCorrect() {
+        if(this.password.length >= 10) {
+            return true;
+        }
+        return false;
+    }
+}
