c77368f4aec0c7b0ef53f28b38ad0ff57611e2b6
BartekB94 <wurir1994@gmail.com>
2024-06-25 12:50:49 +0200

    complete

--- 04/Admin.js
+++ 04/Admin.js
@@ -1,9 +1,6 @@
 import User from './User';
 
 export default class Admin extends User {
-    constructor( {login = '', password = ''}) {
-        super({ login, password })
-    }
     isPasswordCorrect() {
         if(this.password.length >= 10) {
             return true;
--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -14,3 +17,16 @@ const task = {
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+
+const junior = new Junior(features);
+const mid = new Mid(features);
+const senior = new Senior(features);
+
+const juniorTime = junior.getApproximateTimeTaskDoneInHours( task );
+console.log(`Junior: ${juniorTime}`);
+
+const midTime = mid.getApproximateTimeTaskDoneInHours( task );
+console.log(`Mid: ${midTime}`);
+
+const seniorTime = senior.getApproximateTimeTaskDoneInHours( task );
+console.log(`Senior: ${seniorTime}`);
