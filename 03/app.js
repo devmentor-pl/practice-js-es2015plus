90ea6d2995a5a47f4257712eee9b24a3ffe6691a
Piotr Matejek <webesperer@gmail.com>
2024-07-27 23:10:47 +0200

    solveed task 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,24 @@
-import {files} from './data.js';
+import { files } from "./data.js";
 
+const sumOfBytes = (size, unit = "B") => {
+  if (unit === "B") {
+    return size;
+  } else if (unit === "KB") {
+    return size * 1024;
+  } else if (unit === "MB") {
+    return size * 1024 * 1024;
+  } else {
+    return size * 1024 * 1024 * 1024;
+  }
+};
+
+let sum = 0;
+
+files.forEach((item) => {
+  const {
+    size: { length, unit },
+  } = item;
+  sum += sumOfBytes(length, unit);
+});
+
+console.log(sum);
