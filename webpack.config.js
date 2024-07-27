0789c823474b9c718a238aaa3abbf56d63de070b
Piotr Matejek <webesperer@gmail.com>
2024-07-27 21:55:26 +0200

    solved task 01

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const taskNumber = null;
+const taskNumber = "01";
 //numer przerabianego zadania
 
-const path = require('path');
+const path = require("path");
 // importuję bibliotękę [path] z [node.js]
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 // importuję odpowiedni plugin
 module.exports = {
-    entry: `./${taskNumber}/app.js`,
-    // definiuje plik wejściowy
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        // definiuje ścieżką wyjściową
-        filename: 'app.min.js',
-        // definiuję nazwę pliku wyjściowego
-    },
-    target: "web", 
-    // niezbędne do uruchomienia automatycznego odświeżania z webpack-dev-server@3
-    // https://github.com/webpack/webpack-dev-server/issues/2758
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                // określam jakie pliki 
-                // będą brane pod uwagę
-                exclude: /node_modules/,
-                // określam wykluczenia
-                use: 'babel-loader',
-                // określam jaki [loader]
-                // ma być wykorzystany
-            }
-        ]
-        // obecnie brak dodatkowych ustawień
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: `./${taskNumber}/index.html`,
-            // wskazuje plik źródłowy
-            filename: 'index.html'
-            // określan nazwę dla pliku
-        })
-    ]
-}
+  entry: `./${taskNumber}/app.js`,
+  // definiuje plik wejściowy
+  output: {
+    path: path.resolve(__dirname, "build"),
+    // definiuje ścieżką wyjściową
+    filename: "app.min.js",
+    // definiuję nazwę pliku wyjściowego
+  },
+  target: "web",
+  // niezbędne do uruchomienia automatycznego odświeżania z webpack-dev-server@3
+  // https://github.com/webpack/webpack-dev-server/issues/2758
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // określam jakie pliki
+        // będą brane pod uwagę
+        exclude: /node_modules/,
+        // określam wykluczenia
+        use: "babel-loader",
+        // określam jaki [loader]
+        // ma być wykorzystany
+      },
+    ],
+    // obecnie brak dodatkowych ustawień
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./${taskNumber}/index.html`,
+      // wskazuje plik źródłowy
+      filename: "index.html",
+      // określan nazwę dla pliku
+    }),
+  ],
+};
 // eksportuję ustawienia dla webpack-a
