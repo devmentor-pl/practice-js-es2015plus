ac90caff66ec91ed98af8d404f6d77434c5f8f23
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-08-24 17:56:12 +0200

    Add task 05

---
+++ 05/classes/Senior.js
@@ -0,0 +1,19 @@
+import Programmer from "./Programmer.js";
+
+export default class Senior extends Programmer {
+  constructor({ skills, experience, willingness }) {
+    super({ skills, experience, willingness });
+  }
+
+  getMinMaxSkills() {
+    return [50, 90];
+  }
+
+  getMinMaxExperience() {
+    return [60, 90];
+  }
+
+  getMinMaxWilligness() {
+    return [40, 60];
+  }
+}
