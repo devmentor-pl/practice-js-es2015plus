ea03b1bf91a6b99b9182101cee485a15518c4df2
Dagmara Opalka <dagmara-opalka@wp.pl>
2024-09-26 16:50:31 +0200

    finish commit

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,17 @@
 import {files} from './data.js';
 
+const units = {
+    B: 1,
+    KB: 1024,
+    MB: 1024 * 1024,
+    GB: 1024 * 1024 * 1024,
+};
+
+const convertToBytes = ({ length, unit = 'B' }) => length * units[unit];
+
+const calculateTotalSize = (fileList) => {
+    return fileList.reduce((total, { size }) => total + convertToBytes(size), 0);
+};
+
+const totalSize = calculateTotalSize(files);
+console.log(`Total size: ${totalSize} bytes`);
--- 05/app.js
+++ 05/app.js
@@ -1,16 +1,21 @@
-import Programmer from './classes/Programmer';
-
-const features = {
-    skills: 10, 
-    experience: 10, 
-    willingness: 10,
-}
+import Programmer from './classes/Programmer.js';
+import Junior from './classes/Junior.js';
+import Mid from './classes/Mid.js';
+import Senior from './classes/Senior.js';
 
 const task = {
-    difficult: 1,
-    size: 1,
-}
+    difficult: 2,
+    size: 2,
+};
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
-console.log(time);
+const junior = new Junior();
+const juniorTime = junior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Junior time: ${juniorTime} hours`);
+
+const mid = new Mid();
+const midTime = mid.getApproximateTimeTaskDoneInHours(task);
+console.log(`Mid time: ${midTime} hours`);
+
+const senior = new Senior();
+const seniorTime = senior.getApproximateTimeTaskDoneInHours(task);
+console.log(`Senior time: ${seniorTime} hours`);
