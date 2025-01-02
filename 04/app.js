e758232102297dbf0720a0a197dd292bbea2da27
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-02 13:51:31 +0100

    Task 04

--- 04/app.js
+++ 04/app.js
@@ -1,3 +1,4 @@
+import Admin from './Admin.js';
 import User from './User.js';
 
 const userData = { login: 'user@devmentor.pl', password: '123456' }
@@ -5,3 +6,9 @@ const adminData = { login: 'admin@devmentor.pl', password: '1234567890' }
 
 const user = new User( userData );
 user.register();
+
+const admin1 = new Admin( userData );
+admin1.register(); // error
+
+const admin2 = new Admin( adminData );
+admin2.register(); // done
