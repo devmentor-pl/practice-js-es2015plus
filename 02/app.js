087311cb366139ed013643e464908a566673777b
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-02 01:47:46 +0100

    Task 02

---
+++ 02/app.js
@@ -0,0 +1,32 @@
+const quantityFields = document.getElementsByClassName("cart__quantity");
+const btnCalculate = document.querySelector(".cart__btn-calculate");
+const totalPriceSpan = document.querySelector(".cart__total-price");
+
+const validateFields = (...fields) => {
+  return fields.every((field) => {
+    const { value } = field;
+    return value.trim() && !isNaN(+value) && value >= 0;
+  });
+};
+
+btnCalculate.addEventListener("click", () => {
+  let sum = 0;
+  const quantityFieldsList = [...quantityFields];
+
+  const areFieldsValid = validateFields(...quantityFieldsList);
+
+  if (!areFieldsValid) {
+    return alert("Provide valid fields!");
+  }
+
+  quantityFieldsList.forEach((field) => {
+    const {
+      value,
+      dataset: { price },
+    } = field;
+
+    sum += value * price;
+  });
+
+  totalPriceSpan.textContent = sum;
+});
