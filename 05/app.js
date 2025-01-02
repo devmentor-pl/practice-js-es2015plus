523ec5c464e34c25ac81975b44b2cb2965b31da0
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-02 15:03:39 +0100

    Task 05

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -11,6 +14,21 @@ const task = {
     size: 1,
 }
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
-console.log(time);
+// const programmer = new Programmer( features );
+// const time = programmer.getApproximateTimeTaskDoneInHours( task );
+// console.log(time);
+
+const programmers = [new Junior(), new Mid(), new Senior()];
+
+for (let i = 1; i <= 3; i++) {
+    const newTask = {...task, difficult: i};
+    console.log(`Task difficulty: ${i}`);
+
+    programmers.forEach(programmer => {
+        const time = programmer.getApproximateTimeTaskDoneInHours( newTask );
+        console.log(`Average time for ${programmer.constructor.name.toUpperCase()}: ${time}`);
+    });
+    
+    console.log('\n');    
+}
+
