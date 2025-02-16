84c15c21cd814b0692f3857ba8682a466be28b41
marrcelp <marcel.piaszczyk@gmail.com>
2025-02-16 13:45:51 +0100

    delete constructor in Junior, Mid and Senior Class

--- 05/classes/Senior.js
+++ 05/classes/Senior.js
@@ -2,11 +2,6 @@ import Programmer from "./Programmer";
 
 export default class Senior extends Programmer{
 
-    constructor( {skills = null, experience = null, willingness = null} = {} ) {
-
-        super({skills, experience, willingness})
-    }
-
     getMinMaxSkills() {
         return [50, 90];
     }
