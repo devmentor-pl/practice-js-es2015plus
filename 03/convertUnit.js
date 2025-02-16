bf33948beead0c04ace888cae13851f81f7a074f
marrcelp <marcel.piaszczyk@gmail.com>
2025-02-16 13:15:07 +0100

    add else condition to convertUnit.js

--- 03/convertUnit.js
+++ 03/convertUnit.js
@@ -1,11 +1,15 @@
 const convertUnit = (unit, length) => {
 
-    if (unit === 'KB'){
+    if (unit === 'MB'){
+
+    } else if (unit === 'KB'){
         length = length/1024;
     } else if (unit === 'GB'){
         length = length*1024
     } else if (unit === 'B'){
         length = length/1048576;
+    } else {
+        alert(`Niepoprawna jednostka: ${unit}! Podaj wagę pliku w B, KB, MB lub GB!`);
     }
     return length;
 }
