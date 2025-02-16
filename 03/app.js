e84e5c342f9437e506d8afa73bcf83bc872ac7db
marrcelp <marcel.piaszczyk@gmail.com>
2025-02-16 13:20:52 +0100

    add return to calculateLenth function

--- 03/app.js
+++ 03/app.js
@@ -1,15 +1,17 @@
 import {files} from './data.js';
 import convertUnit from './convertUnit.js';
 
-let totalLength = 0;
+
 
 const calculateLength = (files) => {
+    let totalLength = 0;
 
     files.forEach(({ name, size: {length, unit = 'B'}}) => {
         length = convertUnit(unit, length);
         totalLength += length;
     })
-    console.log(`Suma przestrzeni zajmowanej przez wszystkie pliki wynosi ${totalLength.toFixed(2)} MB`);
+    return totalLength;
 }
 
-calculateLength(files);
+const totalLength = calculateLength(files);
+console.log(`Suma przestrzeni zajmowanej przez wszystkie pliki wynosi ${totalLength.toFixed(2)} MB`);
