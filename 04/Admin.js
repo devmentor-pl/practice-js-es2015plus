b7845073fee8f42d05db51cf9549adde5291310d
marrcelp <marcel.piaszczyk@gmail.com>
2025-02-16 13:39:23 +0100

    delete constructor in Admin

--- 04/Admin.js
+++ 04/Admin.js
@@ -2,10 +2,6 @@ import User from "./User";
 
 export default class Admin extends User {
 
-    constructor( {login = '', password = ''} ){
-        super({login, password})
-    }
-
     isPasswordCorrect() {
         if(this.password.length >= 10) {
             return true;
