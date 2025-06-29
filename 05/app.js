479940ed178098fd5fea60ef9ca62e2fed3d650e
Neil Gorski <neil.gorski@hotmail.ch>
2025-06-29 21:42:22 +0200

    05-Done

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
 import Programmer from './classes/Programmer';
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
+import Senior from './classes/Senior';
 
 const features = {
     skills: 10, 
@@ -14,3 +17,16 @@ const task = {
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+
+const junior = new Junior();
+const timeJunior = junior.getApproximateTimeTaskDoneInHours( task );
+console.log(`Junior needs:${timeJunior}h for this task.`)
+
+
+const mid = new Mid();
+const timeMid = mid.getApproximateTimeTaskDoneInHours( task );
+console.log(`Mid needs:${timeMid}h for this task.`)
+
+const senior = new Senior();
+const timeSenior = senior.getApproximateTimeTaskDoneInHours( task );
+console.log(`Senior needs:${timeSenior}h for this task.`)
